763628b8e05aaed8645b94061da4911dfb4b7096
ace020 <895047954@qq.com>
2025-03-10 20:35:05 +0800

    fix: 修复allure打印问题

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 import allure
 
@@ -7,4 +8,6 @@ def pytest_control_run_step(step):
     num1 = random.random()
     num2= random.random()
     allure.attach(str({"test:1"}), "返回数据")
+    time.sleep(5)
     assert  num1 >= num2
+
